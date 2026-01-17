ecef96fb8636ff1e3c2a3400cd299bcce758ef75
eris makolli <erismakolliii@gmail.com>
2026-01-17 14:48:02 +0100

    index.php

---
+++ moduli2/index.php
@@ -0,0 +1,27 @@
+<?php
+$school_Name = "digital school";
+echo ("i go to $school_Name ")."<hr>" ;
+
+$numri = 2 ;
+
+echo ("i am in level $numri")."<br>" ;
+
+$the_String ="ju jeni klasa me e mira";
+ echo strlen($the_String)."<hr>";
+
+ echo str_word_count($the_String)."<hr>" ;
+
+ echo str_replace("mira","zhurmshja", $the_String)."<hr>";
+
+ echo strrev($the_String)."<hr>";
+
+
+
+
+
+
+
+
+
+
+?>
